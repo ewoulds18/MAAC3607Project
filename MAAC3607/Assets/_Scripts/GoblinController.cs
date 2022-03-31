38f080ee80ffc0d1d43fceabb18b28742c58d42a
EricWoulds18 <45571669+ewoulds18@users.noreply.github.com>
2022-03-31 15:38:38 -0300

    before navmesh

--- MAAC3607/Assets/_Scripts/GoblinController.cs
+++ MAAC3607/Assets/_Scripts/GoblinController.cs
@@ -4,35 +4,32 @@ using UnityEngine;
 
 public class GoblinController : MonoBehaviour{
     public int health = 30;
-    public bool isFollowing = false;
-    public int maxDistance = 10;
-    public int minDistance = 2;
-    public int speed = 5;
+    public AudioClip[] goblinSounds;
+    int goblinSoundTimer;
+
+    void Start(){
+        goblinSoundTimer = (int) Random.Range(1 , 2);
+    }
 
     // Update is called once per frame
     void Update(){
         if(health <= 0){
             Destroy(this.gameObject);
         }
-        if(isFollowing){
-            FollowPlayer();
-        }
-    }
+        
+    }   
     
+    //function to decrease health by damage amount
     public void TakeDamage(int damage){
         health -= damage;
         Debug.Log("Goblin takes " + damage + " damage");
     }
 
-    private void FollowPlayer(){
-        //Get player position
-        Transform playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
-        transform.LookAt(playerPosition);
-        if(Vector3.Distance(playerPosition.position, transform.position) > maxDistance){
-            speed = 0;
-        }else{
-            transform.position += transform.forward * speed * Time.deltaTime;
-        }
+    //function ot play a random sound from the goblin's list of sounds
+    private void PlayRandomSound(){
+        int soundIndex = Random.Range(0, goblinSounds.Length);
+        AudioSource.PlayClipAtPoint(goblinSounds[soundIndex], transform.position);
+        Debug.Log("Goblin played sound " + soundIndex);
     }
-    // Follow player using astar pathfinding
+
 }
